523ac56f34c25ebe219dce82f222553f65c440bc
Koushik <Koushik.saha666@gmail.com>
2018-07-14 21:00:57 +0600

    Hello Word API 1st commit

--- Src/app.py
+++ Src/app.py
@@ -1,8 +1,14 @@
 __author__ = 'Koushik'
 
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route(./)
+
+@app.route('/')
+def hello_method():
+    return "Hello, World!"
+
+
+if __name__ == '__main__':
+    app.run(port=4995)
