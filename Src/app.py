7b6668e9f007d738c403d218d9c1e9e2e0912f58
Koushik <Koushik.saha666@gmail.com>
2018-07-19 00:12:40 +0600

    Login Page Show With terminal registration

--- Src/app.py
+++ Src/app.py
@@ -1,13 +1,36 @@
+from pymongo.database import Database
+
+from Src.models.user import User
+
 __author__ = 'Koushik'
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "Koushik"
 
 
 @app.route('/')
 def hello_method():
     return render_template('login.html')
+
+
+@app.before_first_request
+def initialize_database():
+    Database.initialize()
+
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    if User.login_valid(email, password):
+        User.login(email)
+    else:
+        session['email'] = None
+
+    return render_template("profile.html", email=session['email'])
 
 
 if __name__ == '__main__':
