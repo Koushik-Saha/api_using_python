1778ca9a039799fc9032b591a3106cc9e60e6e01
Koushik <Koushik.saha666@gmail.com>
2018-07-18 21:26:33 +0600

    Login And Registration With HTML & CSS

--- Src/app.py
+++ Src/app.py
@@ -1,13 +1,13 @@
 __author__ = 'Koushik'
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_method():
-    return "Hello, World!"
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
